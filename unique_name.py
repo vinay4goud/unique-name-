5561c196312cc7596cd0110ad19e5154315478c5
vinay murigadi <vinay4goud@gmail.com>
2019-02-05 15:01:04 +0530

    Create unique_name.py

---
+++ unique_name.py
@@ -0,0 +1,14 @@
+# unique names
+students  = [ 30, 40, 50, 30, 40]
+
+stu = set(students)
+
+print (stu)
+
+
+student_name  =  ['vinay', 'navin', 'vinay', 'uday', ]
+
+stu_name= set(student_name)
+
+print (stu_name) 
+
